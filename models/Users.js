2c73f02e3fa8feb5400969d2ef2df01beb392d5d
Elvis <elvisgarcia93@gmail.com>
2022-10-26 18:01:07 -0400

    Define the Users Database

---
+++ models/Users.js
@@ -0,0 +1,34 @@
+const { Sequelize } = require("sequelize");
+const { database } = require("../db");
+
+const {database} = require("../db");
+const {Sequelize} = require("sequelize");
+
+
+const Users = demoDB.define("customers", {
+    //id
+    id: {
+        type: Sequelize.INTEGER, // INT
+        autoIncrement: true, // AUTO_INCREMENT
+        primaryKey: true // PRIMARY KEY
+    },
+    // first_name
+    first_name: {
+        type: Sequelize.STRING, // VARCHAR(x)
+        defaultValue: "John" // DEFAULT "John"
+    },
+    // last_name
+    last_name: {
+        type: Sequelize.STRING, // VARCHAR(x)
+        defaultValue: "Doe" // DEFAULT "Doe"
+    },
+    // age
+    email: {
+        type: Sequelize.STRING, // INT
+        allowNull: false // NOT NULL
+    }
+});
+
+module.exports = {
+    Customer
+}
