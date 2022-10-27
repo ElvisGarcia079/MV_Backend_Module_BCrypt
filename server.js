db67f0c4add8aab8ce279cfc97d4f5981bbf77ad
Elvis <elvisgarcia93@gmail.com>
2022-10-26 21:30:40 -0400

    Import the User and Password Modules

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const {User, Password} = require("./models/index");
+
 const port = 3020;
 
 app.use(express.json());
@@ -8,12 +10,12 @@ app.use(express.urlencoded({extended:true}));
 
 app.get("/", (req, res) => {
     res.send("Successful GET Request Made to the Bcrypt server");
-})
+});
 
 app.get("/users", async (req, res) => {
-    
+
     res.send();
-})
+});
 
 app.listen(port, () => {
     console.log(`App listening on http://localhost:${port}`);
