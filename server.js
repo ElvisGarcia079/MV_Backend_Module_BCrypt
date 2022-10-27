fe6280a894a779e37808d759742004dcee5d1a4b
Elvis <elvisgarcia93@gmail.com>
2022-10-26 21:25:11 -0400

    Include Middleware for Express to parse JSON and URL Encoded Forms

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 
 const port = 3020;
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.listen(port, () => {
     console.log(`App listening on http://localhost:${port}`);
 });
